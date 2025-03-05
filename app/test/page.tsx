820bfe48ab7791aa87105e9aaae3341f018bb76f
Kadacheahmedrami <a_kadache@estin.dz>
2025-03-05 01:27:12 +0100

    start debuging the Star error

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,28 +1,17 @@
-import MazeRenderer from "@/components/td-comps/MazeRenderer"
-import type { MazeGrid } from "@/app/types/maze"
 
-// Example maze layout:
-// 0: Empty space
-// 1: Wall
-// 2: Start point
-// 3: End point
-const exampleMaze: MazeGrid = [
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 , 1],
-  [1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 3 , 1],
-  [1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0 , 1],
-  [1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0 , 1],
-  [1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0 , 1],
-  [1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0 , 1],
-  [1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1 , 1],
-  [1, 2, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0 , 1],
-  [1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0 , 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 , 1],
+import {aStar} from "@/app/utils/tp01Utils/astar"
+import {getEdges,getNodes} from "@/app/utils/tp01Utils/getData"
 
-]
 
 export default function Home() {
+    const nodes = getNodes().nodes
+    const edges = getEdges().edges
+
+    const result = aStar({graph : {nodes,edges} , start:"Batna" , goal:"Mostaganem"})
+    console.log(result)
   return (
-    <MazeRenderer grid={exampleMaze} cellSize={1.2} />
+
+  <></>
   
   )
 }
